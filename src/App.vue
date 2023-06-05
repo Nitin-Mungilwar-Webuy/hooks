89c941fc345203a936aa6a3f8056027c7d4bcac3
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-06-05 17:22:38 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  ref,
+} from "vue";
+import Hello from "./components/Hello.vue";
+const showHelloComp = ref(true);
+const count = ref(0);
+
+onBeforeMount(() => {
+  console.log("onBeforeMount called-parent");
+});
+onMounted(() => {
+  console.log("onMounted called-parent");
+});
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate called-parent");
+});
+onUpdated(() => {
+  console.log("onUpdated called-parent");
+});
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount called-parent");
+});
+onUnmounted(() => {
+  console.log("onUnmounted called-parent");
+});
+</script>
+
+<template>
+  <h1>App component parent</h1>
+  count: {{ count }}
+  <Hello v-if="showHelloComp" />
+  <button @click="showHelloComp = !showHelloComp">show/Hide</button>
+  <button @click="count++">Increment</button>
+</template>
+
+<style scoped></style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+
+onBeforeMount(() => {
+  console.log("onBeforeMount called-child");
+});
+onMounted(() => {
+  console.log("onMounted called-child");
+});
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate called-child");
+});
+onUpdated(() => {
+  console.log("onUpdated called-child");
+});
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount called-child");
+});
+onUnmounted(() => {
+  console.log("onUnmounted called-child");
+});
+</script>
+<template><h1>hello component child</h1></template>
+<style></style>
